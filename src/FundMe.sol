ef4877cab3649919037ecef0ef0c6fb15e7cc909
Ali Yasin Emir <aliyasin.emir16@gmail.com>
2023-10-17 20:46:46 +0300

    Modify error function by adding the contract name

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
-error NotOwner();
+error FundMe__NotOwner();
 
 contract FundMe {
     using PriceConverter for uint256;
